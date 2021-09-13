724990f3a44d19ec017559bb7ea08369178e8315
janna <rijanuy88@gmail.com>
2021-09-13 23:50:46 +0800

    added: sample image in cards

--- resources/js/Components/Map/Card.vue
+++ resources/js/Components/Map/Card.vue
@@ -3,14 +3,22 @@
         <button type="button" class="btn btn-light">
             <div class="row g-0 mh-15">
                 <div class="container col-md-3">
-                    <img src="https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg" class="img-fluid rounded-start" alt="https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg" style="object-fit:cover">
+                    <img src="https://cf.shopee.ph/file/2dfba819d401bc834927518570cfc877" class="img-fluid rounded-start" alt="https://hatrabbits.com/wp-content/uploads/2017/01/random.jpg" style="object-fit:cover">
                 </div>
                 <div class="col-md-8 align-items-start">
                     <div class="p-3 text-start">
-                        <p class="card-title">District: </p>
-                        <h5 class="card-text">Ateneo de Davao University</h5>
-                        <p class="card-text"><small class="text-muted">Address</small></p>
-                        <p class="card-text"><small class="text-muted">Tel.no.</small></p>
+                        <div>
+                            <p class="card-title">District: </p>
+                        </div>
+                        <div>
+                            <h5 class="card-text">Ateneo de Davao University</h5>
+                        </div>
+                        <div>
+                            <p class="card-text"><small class="text-muted">Address</small></p>
+                        </div>
+                        <div>
+                            <p class="card-text"><small class="text-muted">Tel.no.</small></p>
+                        </div>
                     </div>
                 </div>
                 <div class="col-md-1 row-2 bg-primary text-white"></div>

5627c4059206e318a88411eb3c92b836b287ca2b
MTHLily <tree.of.pyres@gmail.com>
2021-09-17 20:49:14 +0900

    Configured postgresql for heroku

--- config/database.php
+++ config/database.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Str;
 
+if( env("APP_ENV") == "production"){
+    $url = parse_url( env("DATABASE_URL") );
+    $host = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $database=substr($url["path"], 1);
+}
+
 return [
 
     /*
@@ -15,7 +23,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,11 +74,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => $host || env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => $database || env('DB_DATABASE', 'forge'),
+            'username' => $username || env('DB_USERNAME', 'forge'),
+            'password' => $password || env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,

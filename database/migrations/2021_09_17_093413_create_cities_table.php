924ae24e153148edd0ecf5f13bbd98921a950285
MTHLily <tree.of.pyres@gmail.com>
2021-09-17 22:07:07 +0900

    Small bug with city migration

--- database/migrations/2021_09_17_093413_create_cities_table.php
+++ database/migrations/2021_09_17_093413_create_cities_table.php
@@ -15,7 +15,7 @@ class CreateCitiesTable extends Migration
     {
         Schema::create('cities', function (Blueprint $table) {
             $table->id();
-            $table->name();
+            $table->string('name');
             $table->timestamps();
         });
     }

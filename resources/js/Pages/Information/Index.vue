bdabd2e28036b9a0122d0ce7aa23c490b4d76ac6
janna <rijanuy88@gmail.com>
2021-09-19 22:45:08 +0800

    modal styles

--- resources/js/Pages/Information/Index.vue
+++ resources/js/Pages/Information/Index.vue
@@ -68,14 +68,16 @@
                         <form @submit.prevent="submitDelete(title.id)">
                           <div class="modal-dialog">
                             <div class="modal-content">
-                              <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Delete Item</h5>
+                              <div class="modal-header" style="background-color:#DADEEE">
+                                <h5 class="modal-title text-uppercase" id="exampleModalLabel"><i class="bi bi-exclamation-circle-fill" style="color:#DC3545" ></i> Delete Item</h5>
                                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                               </div>
-                              <div class="modal-body">
-                                Are you sure you want to delete item?
+                              <div class="modal-body text-start" style="height:80px">
+                                  <h6 class="p-3">
+                                    Are you sure you want to delete item?
+                                  </h6>
                               </div>
-                              <div class="modal-footer">
+                              <div class="modal-footer" style="background-color:#DADEEE">
                                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
                                 <button type="submit" class="btn btn-danger" data-bs-dismiss="modal" aria-label="Close">Delete</button>
                               </div>

e88e11f49e5df1b038e7b76bcf2c4948483b4a68
janna <rijanuy88@gmail.com>
2021-09-22 21:08:57 +0800

    added: MapPicker districts form

--- resources/js/Components/Districts/DistrictForm.vue
+++ resources/js/Components/Districts/DistrictForm.vue
@@ -39,15 +39,60 @@
       ></ColorPicker>
       <div class="invalid-feedback">{{ district.errors.color }}</div>
     </div>
+    <div class="mb-3 p-3" style="background-color: #FFFFFF;">
+      <div class="row">
+        <div class="col">
+          <div class="mb-3">
+            <label for="longitude" class="form-label">Longitude</label>
+            <input
+              v-model="district.longitude"
+              type="text"
+              class="form-control bg-white"
+              :class="{
+                'is-invalid': district.errors.longitude,
+              }"
+              id="longitude"
+              readonly
+              placeholder="Longitude"
+            />
+            <div class="invalid-feedback">{{ district.errors.longitude }}</div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="mb-3">
+            <label for="latitude" class="form-label">Latitude</label>
+            <input
+              v-model="district.latitude"
+              type="text"
+              class="form-control bg-white"
+              readonly
+              :class="{
+                'is-invalid': district.errors.latitude,
+              }"
+              id="latitude"
+              placeholder="Latitude"
+            />
+            <div class="invalid-feedback">{{ district.errors.latitude }}</div>
+          </div>
+        </div>
+      </div>  
+      <MapPicker
+          v-model:longitude="district.longitude"
+          v-model:latitude="district.latitude"
+          id="mapPicker"
+      ></MapPicker>
+    </div>
   </div>
 </template>
 
 <script>
 import ColorPicker from "../ColorPicker.vue";
+import MapPicker from "../Map/MapPicker.vue";
 
 export default {
   components: {
     ColorPicker,
+    MapPicker, 
   },
   props: {
     district: {
@@ -58,7 +103,11 @@ export default {
         errors: {
           question: "",
           answer: "",
+          longitude: "",
+          latitude: "",
         },
+        longitude: 125.61315274255003,
+        latitude: 7.071250196247246,
       }),
     },
   },

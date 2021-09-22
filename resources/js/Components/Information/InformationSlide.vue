e19a3ab8a43b838984e8e22a2c2b8fdb3cf5c838
janna <rijanuy88@gmail.com>
2021-09-22 22:06:50 +0800

    Added: carousel styling

--- resources/js/Components/Information/InformationSlide.vue
+++ resources/js/Components/Information/InformationSlide.vue
@@ -2,10 +2,10 @@
   <Slide>
     <div
       class="bg-primary text-white vw-100 p-5"
-      style="height: clamp(200px, 60vh, 1000px);background-size: cover;background-repeat: no-repeat;background-position: center;background: linear-gradient( rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7) ), url('https://scontent.fmnl8-2.fna.fbcdn.net/v/t1.6435-9/65932238_2354332941290721_9038293276070772736_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=973b4a&_nc_ohc=zGMPHZYV8E8AX-EcwRS&_nc_ht=scontent.fmnl8-2.fna&oh=fe3f8b351ab6dd028e4bc323b591cce1&oe=616D8039'); "
+      style="height: clamp(200px, 100vh, 1000px);background: linear-gradient( rgba(8, 34, 141, 0.7), rgba(8, 34, 141, 0.7) ), url('https://scontent.fmnl8-2.fna.fbcdn.net/v/t1.6435-9/65932238_2354332941290721_9038293276070772736_n.jpg?_nc_cat=111&ccb=1-5&_nc_sid=973b4a&_nc_ohc=zGMPHZYV8E8AX-EcwRS&_nc_ht=scontent.fmnl8-2.fna&oh=fe3f8b351ab6dd028e4bc323b591cce1&oe=616D8039'); background-size:cover; background-repeat: no-repeat; background-position: center;"
       
     >
-      <h1>{{ info.title }}</h1>
+      <h1 style="margin-top:15%;font-size: 4em;font-weight:bold">{{ info.title }}</h1>
       <!-- <div class="text-white">
         <MarkdownViewer
           :content="info.content"
@@ -13,7 +13,7 @@
         ></MarkdownViewer>
       </div> -->
       <!-- <p class="lead">{{ info.content }}</p> -->
-      <div v-html="markedContent"></div>
+      <div v-html="markedContent" style="font-size: 2em;"></div>
     </div>
   </Slide>
 </template>

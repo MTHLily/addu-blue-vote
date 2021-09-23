a8629e517fa05d44bfce71e9ffd5469e9394a22f
MTHLily <tree.of.pyres@gmail.com>
2021-09-23 11:06:40 +0900

    Fixed small bug with database

--- config/database.php
+++ config/database.php
@@ -71,13 +71,17 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', 'localhost'),
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
+            'prefix_indexes' => true,
             'schema' => 'public',
+            'sslmode' => 'prefer',
         ],
 
         'sqlsrv' => [

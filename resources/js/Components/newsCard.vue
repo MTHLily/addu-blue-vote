f179ab77b932ad638d0b469f9adf06736fcab866
janna <rijanuy88@gmail.com>
2021-10-18 16:51:48 +0800

    added: color props

--- resources/js/Components/newsCard.vue
+++ resources/js/Components/newsCard.vue
@@ -3,7 +3,7 @@
         <div class="p-3 mb-2 mt-2 mx-2 " style="width: 50%;">
             <div class="card p-3 mb-2" style="border-radius:20px">
                 <h5 class="card-title">{{ news.title }}</h5>
-                <button class="btn btn-primary btn-sm text-white card-subtitle mb-2 col-6" style="border-radius:20px" disabled>{{ news.source }} </button>
+                <button class="btn btn-primary btn-sm text-white card-subtitle mb-2 col-6" style="border-radius:20px" :style="{ 'background-color': news.color }"  disabled>{{ news.source }} </button>
                 <p class="card-text text-wrap"> {{ news.description }}</p>
                 <div class="d-flex justify-content-between">
                     <p class="card-subtitle mb-2 text-muted"> {{ news.date }} </p>
@@ -32,6 +32,7 @@ export default {
             description:"descriptionsssssdescriptionsssssdescriptionsssssdescriptionsssssdescriptionsssssdescriptionsssssdescriptionsssssdescriptionsssssdescriptionsssssdescriptionsssssdescriptionsssssdescriptionsssss",
             date:"Oct 18, 2021",
             link:"https://news.abs-cbn.com/",
+            color:'#064205',
         }),
         },
     },

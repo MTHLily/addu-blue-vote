7f3855e272b3124c1a2c2153165d75c4555db82c
janna <rijanuy88@gmail.com>
2021-11-04 10:15:04 +0800

    added: pagination component

--- resources/js/Pages/VotersEducation.vue
+++ resources/js/Pages/VotersEducation.vue
@@ -6,6 +6,7 @@
       <ElectionProcessInfo></ElectionProcessInfo>
       <h1 class="text-primary fw-bold my-5">Election News and Issues</h1>
       <NewsDisplay :articles="articles"></NewsDisplay>
+      <Pagination />
       <pre>
         {{ articles }}
       </pre>
@@ -17,9 +18,10 @@
 import { defineComponent } from "@vue/runtime-core";
 import NewsDisplay from "../Components/News/NewsDisplay.vue";
 import ElectionProcessInfo from "../Components/ElectionProcessInfo.vue";
+import Pagination from "../Components/Pagination.vue";
 
 export default defineComponent({
-  components: { NewsDisplay, ElectionProcessInfo },
+  components: { NewsDisplay, ElectionProcessInfo, Pagination },
   props: {
     articles: {
       type: Object,

32d893fc88d122a910857a51f22d5f0e0ad32e0f
janna <rijanuy88@gmail.com>
2021-11-04 16:50:41 +0800

    added: regional and district filter

--- resources/js/Components/CandidateProfile/NewsArticle.vue
+++ resources/js/Components/CandidateProfile/NewsArticle.vue
@@ -1,46 +1,49 @@
 <template lang="">
-  <div class="card p-3" style="border-radius: 20px">
-    <h5 class="card-title">{{ news.title }}</h5>
-    <button
-      class="btn btn-primary btn-sm text-white card-subtitle mb-2 col-6"
-      style="border-radius: 20px"
-      :style="{ 'background-color': news.news_source.color || 'blue' }"
-      disabled
-    >
-      {{ news.news_source.name }}
-    </button>
-    <p class="card-text text-wrap">{{ news.description }}</p>
-    <div class="d-flex justify-content-between">
-      <p class="card-subtitle mb-2 text-muted">{{ news.date }}</p>
-      <button type="button" class="btn btn-primary" style="border-radius: 20px">
-        <a
-          :href="`${news.url}`"
-          target="_blank"
-          class="text-white text-decoration-none"
+    <div class="card p-3" style="border-radius: 20px">
+        <h5 class="card-title">{{ newsArticle.title }}</h5>
+        <button
+        class="btn btn-primary btn-sm text-white card-subtitle mb-2 col-6"
+        style="border-radius: 20px"
+        :style="{ 'background-color': newsArticle.news_source.color || 'blue' }"
+        disabled
         >
-          READ MORE
-        </a>
-      </button>
+        {{ newsArticle.news_source.name }}
+        </button>
+        <p class="card-text text-wrap">{{ newsArticle.description }}</p>
+        <div class="d-flex justify-content-between">
+        <p class="card-subtitle mb-2 text-muted">{{ newsArticle.date }}</p>
+        <button type="button" class="btn btn-primary" style="border-radius: 20px">
+            <a
+            :href="`${newsArticle.news_source.home_url}`"
+            target="_blank"
+            class="text-white text-decoration-none"
+            >
+            READ MORE
+            </a>
+        </button>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 export default {
-  props: {
-    news: {
-      type: Object,
-      default: () => ({
-        title: "default title",
-        source: "default source",
-        description:
-          "default description",
-        date: "default date",
-        link: "https://news.abs-cbn.com/",
-        color: "#064205",
-      }),
+    props: {
+        newsArticle: {
+        type: Object,
+        default: () => ({
+            id: '1',
+            title: "default title",
+            description: "default description",
+            url: "https://www.google.com",
+            date: "default date",
+            new_source: {
+                name: "default name",
+                home_url:"https://news.abs-cbn.com/",
+            }
+            
+        }),
+        },
     },
-  },
 };
 </script>
 
--- resources/js/Components/CandidateProfile/NewsArticles.vue
+++ resources/js/Components/CandidateProfile/NewsArticles.vue
@@ -1,74 +1,11 @@
+// TODO:fix newscard import
 <template lang="">
   <div class="container col-6 text-white h-auto" style="background-color: #faa728">
     <div class="card-title p-3 text-uppercase fw-bold lh-sm fs-1">NEWS ARTICLES</div>
-    <div class="card-body">
-      <!-- <NewsCard></NewsCard> -->
-      <div
-        id="carouselExampleControls"
-        class="carousel slide"
-        data-bs-ride="carousel"
-      >
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <!-- <div v-for="article in articles.data" :key="article.id">
-              <div class="col mb-2">
-                <NewsCard :news="article"></NewsCard>
-              </div>
-            </div> -->
-          </div>
-          <div class="carousel-item p-3 mb-3">
-            <div class="card-body bg-white" style="border-radius: 20px">
-              <h5 class="card-title">Card title</h5>
-              <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-              <p class="card-text">SAAAAA.</p>
-              <a href="#" class="card-link">Card link</a>
-              <a href="#" class="card-link">Another link</a>
-            </div>
-            <div class="card-body bg-white" style="border-radius: 20px">
-              <h5 class="card-title">Card title</h5>
-              <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-              <p class="card-text">SAAAAA.</p>
-              <a href="#" class="card-link">Card link</a>
-              <a href="#" class="card-link">Another link</a>
-            </div>
-            <div class="card-body bg-white" style="border-radius: 20px">
-              <h5 class="card-title">Card title</h5>
-              <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-              <p class="card-text">SAAAAA.</p>
-              <a href="#" class="card-link">Card link</a>
-              <a href="#" class="card-link">Another link</a>
-            </div>
-          </div>
-          <div class="carousel-item p-3">
-            <div class="card-body bg-white" style="border-radius: 20px">
-              <h5 class="card-title">Card title</h5>
-              <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
-              <p class="card-text">AAnt.</p>
-              <a href="#" class="card-link">Card link</a>
-              <a href="#" class="card-link">Another link</a>
-            </div>
-          </div>
-        </div>
-        <button
-          class="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleControls"
-          data-bs-slide="prev"
-        >
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
-        </button>
-        <button
-          class="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleControls"
-          data-bs-slide="next"
-        >
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
-        </button>
-      </div>
-    </div>
+      <!-- <template v-for="article in articles.data" :key="article.id">
+        <NewsCard :newsArticle="article"></NewsCard>
+      </template> -->
+      <h1>aaaaaaaaaaaaaaaaaaaaaa</h1>
   </div>
 </template>
 <script>
@@ -87,4 +24,6 @@ export default defineComponent({
 });
 </script>
 
-<style></style>
+<style>
+
+</style>

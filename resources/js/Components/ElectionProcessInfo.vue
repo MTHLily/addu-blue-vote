30f9c6aff2a374fea70038435999d9fd3c99a7f0
ignasstephanie <ignasstephanie@gmail.com>
2021-11-07 00:52:11 +0800

    Changed colors dominant blue: election process

--- resources/js/Components/ElectionProcessInfo.vue
+++ resources/js/Components/ElectionProcessInfo.vue
@@ -22,23 +22,23 @@ export default {
         description:
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         primaryColor: "#055094",
-        secondaryColor: "#ce2029",
+        secondaryColor: "#56A0D7",
       },
       {
         step: "STEP 2",
         title: "Lorem ipsum dolor sit amet",
         description:
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        primaryColor: "#ce2029",
-        secondaryColor: "#faa728",
+        primaryColor: "#379DD2",
+        secondaryColor: "#055094",
       },
       {
         step: "STEP 3",
         title: "Lorem ipsum dolor sit amet",
         description:
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        primaryColor: "#faa728",
-        secondaryColor: "#055094",
+        primaryColor: "#055094",
+        secondaryColor: "#379DD2",
       },
     ],
   }),

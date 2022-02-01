8fe230bfd9e3383e363c4350047fa6608d26f24f
MTHLily <tree.of.pyres@gmail.com>
2022-02-01 17:14:39 +0800

    Feature: Show active featured video

--- resources/js/Components/VideoResources/VideoResourceCard.vue
+++ resources/js/Components/VideoResources/VideoResourceCard.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="card hover:shadow-lg cursor-pointer">
+  <div
+    class="card hover:shadow-lg cursor-pointer"
+    :class="{ 'bg-primary text-white': active }"
+  >
     <div class="card-body">
       <h3 class="card-title fs-5">{{ videoResource.title }}</h3>
       <div class="card-text">{{ videoResource.description }}</div>
@@ -15,6 +18,10 @@ export default defineComponent({
     videoResource: {
       type: Object,
       required: true,
+    },
+    active: {
+      type: Boolean,
+      default: false,
     },
   },
 });

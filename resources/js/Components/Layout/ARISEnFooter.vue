17711d91b7c1271d6f3738393fb3f2ca17e0f971
janna <rijanuy88@gmail.com>
2022-02-16 12:08:21 +0800

    edited footer

--- resources/js/Components/Layout/ARISEnFooter.vue
+++ resources/js/Components/Layout/ARISEnFooter.vue
@@ -1,19 +1,20 @@
 <template>
-  <footer class="text-white bg-primary p-3 mt-4">
+  <footer class="text-white bg-primary p-3 mt-4 mb-0">
     <div class="d-flex justify-content-between container">
       <div>
         <b>Ateneo de Davao University Blue Vote</b> Copyright © 2021 | Developed
         by <b>ARISEn Lab</b>
+        <p class="mb-0">Tel No: +63 (82) 221.2411 local 8262 <br> Fax: +63 (82) 226.4116 </p>
       </div>
       <div class="d-flex gap-2">
-        <a href="https://www.facebook.com/adduarisen/" target="_blank">
+        <a href="https://www.facebook.com/AdDUBlueVote/" target="_blank">
           <i class="bi bi-facebook text-white"></i>
         </a>
-        <a href="https://twitter.com/addu_arisen?lang=en" target="_blank">
+        <a href="https://twitter.com/AdDUBlueVote" target="_blank">
           <i class="bi bi-twitter text-white"></i>
         </a>
         <a
-          href="mailto:arisen.lab@addu.edu.ph?subject=subject text"
+          href="mailto:bluevote@addu.edu.ph?subject=subject text"
           target="_blank"
         >
           <i class="bi bi-google text-white"></i>
